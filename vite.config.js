3258f7812c487edd308c3c3dee78055b2516ea09
Samuel Chamalé <samuel.chamale23@gmail.com>
2023-04-11 18:28:12 -0600

    Some workspace customization
    1. Custom server
    2. Aliasing
    3. Remove wrong formating configuration

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-import eslintPlugin from 'vite-plugin-eslint'
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-import prettierPlugin from 'vite-plugin-prettier'
-
-// https://vitejs.dev/config/
+// docs:: https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin({
-      cache: false,
-      fix: true,
-    }),
-    prettierPlugin(),
-  ],
+  server: {
+    host: '0.0.0.0',
+    port: 4444,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+    ],
+  },
 })
